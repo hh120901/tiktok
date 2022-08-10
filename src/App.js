005cdec9e9344fc871d8310b91701fff4a6ea944
hh120901 <70877597+hh120901@users.noreply.github.com>
2022-08-10 14:09:32 +0700

    testUseEffect

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from "./logo.svg";
+import "./App.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [number, setnumber] = useState(0);
+  const increase = () => (console.log(number), setnumber(number + 1));
+  const decrease = () => (console.log(number), setnumber(number - 1));
+  useEffect(() => {
+    console.log("effect");
+  }, []);
+
   return (
     <div className="App">
-      
+      <h1>REACT JS</h1>
+      <h2>{number || "0"}</h2>
+      <button onClick={increase} style={{ margin: 10 }}>
+        Increase
+      </button>
+      <button onClick={decrease} style={{ margin: 10 }}>
+        Decrese
+      </button>
     </div>
   );
 }
